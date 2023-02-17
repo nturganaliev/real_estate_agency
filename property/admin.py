fadc25b4597abc3d001d36f261feea206f72dcb3
nturganaliev <nurlan.turganaliev@gmail.com>
2023-02-17 22:55:23 +0600

    Add read_only field to admin panel

--- property/admin.py
+++ property/admin.py
@@ -5,5 +5,6 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'price')
+    readonly_fields = ['created_at']
 
 admin.site.register(Flat, FlatAdmin)
