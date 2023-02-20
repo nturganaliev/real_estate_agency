3875de699cb526fe71fc8443b043751bbf9eac45
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-02-20 18:41:42 +0600

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/migrations/0015_flat_owner_pure_phone_number_populate.py
+++ property/migrations/0015_flat_owner_pure_phone_number_populate.py
@@ -7,14 +7,15 @@ from django.db import migrations
 def correct_phonenumbers_format(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        flat.owner_pure_phone = phonenumbers.parse(flat.owners_phonenumber, 'RU')
-        flat.save()
+        if phonenumbers.is_valid_number(phonenumbers.parse(flat.owners_phonenumber, 'RU')):
+            flat.owner_pure_phone = phonenumbers.parse(flat.owners_phonenumber, 'RU')
+            flat.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0013_flat_owner_pure_phone'),
+        ('property', '0014_alter_flat_owner_pure_phone'),
     ]
 
     operations = [
