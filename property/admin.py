3ed0f8692926eb06a1db25d6c0517d70a7a2c0bd
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-02-24 14:47:33 +0600

    Clean unnecessary comments

--- property/admin.py
+++ property/admin.py
@@ -23,8 +23,6 @@ class FlatAdmin(admin.ModelAdmin):
         'address',
         'price',
         'new_building',
-        # 'owners_phonenumber',
-        # 'owner_pure_phone',
     )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
